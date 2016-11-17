583c5d6f997c7a68c6bb87ef0dae960e82eb15f8
Flash Kirby <flashkirby@gmail.com>
2016-11-17 01:47:43 +0000

    Add initial expedition files

---
+++ Expedition.cs
@@ -0,0 +1,69 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace Expeditions
+{
+    public class Expedition
+    {
+        public string title;
+        public string description;
+        public string conditionDescription;
+        public int difficulty;
+        public bool active;
+        public bool important;
+        public bool explore;
+        public bool deliver;
+        public bool defeat;
+        public bool partyShare;
+        private List<KeyValuePair<int, int>> deliverables = new List<KeyValuePair<int, int>>();
+        private List<KeyValuePair<int, int>> rewards = new List<KeyValuePair<int, int>>();
+
+        public bool Completed()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Add an item to be handed in for the expedition to be successful
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="stack"></param>
+        public void AddDeliverable(int type, int stack)
+        {
+            deliverables.Add(new KeyValuePair<int, int>(
+                type,
+                stack
+                ));
+        }
+        public void AddDeliverable(Item item, int stack)
+        {
+            AddDeliverable(item.type, stack);
+        }
+        public void AddDeliverable(ModItem moditem, int stack)
+        {
+            AddDeliverable(moditem.item.type, stack);
+        }
+
+        /// <summary>
+        /// Add an item to be given out to participants who finished the expedition
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="stack"></param>
+        public void AddReward(int type, int stack)
+        {
+            rewards.Add(new KeyValuePair<int, int>(
+                type,
+                stack
+                ));
+        }
+        public void AddReward(Item item, int stack)
+        {
+            AddReward(item.type, stack);
+        }
+        public void AddReward(ModItem moditem, int stack)
+        {
+            AddReward(moditem.item.type, stack);
+        }
+    }
+}
---
+++ ModExpedition.cs
@@ -0,0 +1,50 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace Expeditions
+{
+    public class ModExpedition
+    {
+        public readonly Mod mod;
+
+        public ModExpedition(Mod mod)
+        {
+            this.mod = mod;
+        }
+
+        public virtual bool Autoload(ref string name)
+        {
+            return this.mod.Properties.Autoload;
+        }
+
+        #region Virtual Methods
+        /// <summary>
+        /// The initialisation method for mods using this. Use it to set the title, rewards etc.
+        /// </summary>
+        public virtual void SetDefaults()
+        {
+
+        }
+
+        /// <summary>
+        /// Put in any checks here to determine whether the expedition is complete, sans deliverables
+        /// </summary>
+        /// <returns>True if conditions are met</returns>
+        public virtual bool CheckConditions()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Put in any checks here to determine whether the expedition is visible yet
+        /// </summary>
+        /// <returns>True if prerequisites are met</returns>
+        public virtual bool CheckPrerequisites()
+        {
+            return true;
+        }
+        #endregion
+
+    }
+}
