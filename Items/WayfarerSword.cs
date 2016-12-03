a36bd98bac3006f07c999f7bf5ad28de10633a44
Flash Kirby <flashkirby@gmail.com>
2016-12-03 18:45:23 +0000

    Reduce lines via CloneDefaults()

--- Items/WayfarerSword.cs
+++ Items/WayfarerSword.cs
@@ -1,4 +1,5 @@
 ﻿using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Expeditions.Items
@@ -7,14 +8,13 @@ namespace Expeditions.Items
     {
         public override void SetDefaults()
         {
+            item.CloneDefaults(ItemID.GoldBroadsword);
             item.name = "Wayfarer's Sword";
             item.width = 32;
             item.height = 36;
             item.scale = 0.95f;
             item.useSound = 1;
-
-            item.useStyle = 1;
-            item.melee = true;
+            
             item.damage = 16;
             item.useAnimation = 21;
             item.knockBack = 6f;
