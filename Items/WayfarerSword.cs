06fcd84d98bad1f3476f4703d95465c27800f12c
Flash Kirby <flashkirby@gmail.com>
2016-12-03 18:40:34 +0000

    Add code and stats

---
+++ Items/WayfarerSword.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Expeditions.Items
+{
+    public class WayfarerSword : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Wayfarer's Sword";
+            item.width = 32;
+            item.height = 36;
+            item.scale = 0.95f;
+            item.useSound = 1;
+
+            item.useStyle = 1;
+            item.melee = true;
+            item.damage = 16;
+            item.useAnimation = 21;
+            item.knockBack = 6f;
+            
+            item.value = Item.sellPrice(0, 0, 25, 0);
+        }
+    }
+}
