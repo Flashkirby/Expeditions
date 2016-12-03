df29fd15b732de80b2b0e224100facd75d2756be
Flash Kirby <flashkirby@gmail.com>
2016-12-03 18:45:41 +0000

    Add Pike code

---
+++ Items/WayfarerPike.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Expeditions.Items
+{
+    public class WayfarerPike : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.Spear);
+            item.name = "Wayfarer's Pike";
+            item.width = 32;
+            item.height = 36;
+            
+            item.damage = 8;
+            item.useAnimation = 28;
+            item.useTime = 28;
+            item.knockBack = 5.5f;
+            item.shoot = mod.ProjectileType("WayfarerPike");
+            item.shootSpeed = 3.6f;
+
+            item.value = Item.sellPrice(0, 0, 25, 0);
+        }
+    }
+}
