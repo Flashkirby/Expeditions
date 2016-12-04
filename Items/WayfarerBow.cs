c5bfdceaaa1573aa15b31ab67ccc68329639eed3
Flash Kirby <flashkirby@gmail.com>
2016-12-04 00:48:00 +0000

    Add Wayfarer's Bow code

---
+++ Items/WayfarerBow.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Expeditions.Items
+{
+    public class WayfarerBow : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.GoldBow);
+            item.name = "Wayfarer's Bow";
+
+            item.damage = 10;
+            item.useAnimation = 25;
+            item.knockBack = 2f;
+            item.shootSpeed = 6.6f;
+
+            item.value = Item.sellPrice(0, 0, 25, 0);
+        }
+    }
+}
