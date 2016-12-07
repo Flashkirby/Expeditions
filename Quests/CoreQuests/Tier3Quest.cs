08b5975737bbb762319f91e825d1924544511959
Flash Kirby <flashkirby@gmail.com>
2016-12-07 22:53:26 +0000

    Add tier 3 quest

--- Quests/CoreQuests/Tier3Quest.cs
+++ Quests/CoreQuests/Tier3Quest.cs
@@ -9,22 +9,33 @@ namespace Expeditions.Quests
     {
         public override void SetDefaults()
         {
-            expedition.name = "Placeholder3";
+            expedition.name = "Dungeon Admission";
             expedition.difficulty = 3;
+            expedition.defeat = true;
             expedition.important = true;
+
+            expedition.conditionDescription1 = "Gain access to the Dungeon";
+
+            AddRewardItem(ItemID.GoldCoin, 2);
+            AddRewardItem(ItemID.GoldenKey, 1);
+            AddRewardItem(ItemID.AnkletoftheWind, 1);
+            AddRewardPrefix(ItemID.HealingPotion, 15);
+            AddRewardPrefix(ItemID.ManaPotion, 15);
+            AddRewardPrefix(ItemID.Bomb, 10);
         }
         public override string Description(bool complete)
         {
-            return "";
-        }
-        public override void AddItemsOnLoad()
-        {
-
+            return "Sooner or later we'll need access to the dungeon so we can map it out. That's where you come in! Maybe try asking that old guy nicely? ";
         }
 
         public override bool CheckPrerequisites(Player player)
         {
             return Expeditions.GetCurrentExpeditionTier() >= expedition.difficulty - 1;
+        }
+        public override bool CheckConditions(Player player, ref bool condition1, ref bool condition2, ref bool condition3)
+        {
+            if (NPC.downedBoss2 && !condition1) condition1 = player.ZoneDungeon;
+            return condition1;
         }
     }
 }
