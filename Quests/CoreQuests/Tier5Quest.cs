261419c60c19e7ba8a9da8ba22cd33c0a905a0ea
Flash Kirby <flashkirby@gmail.com>
2016-12-08 01:04:55 +0000

    Add tier 5 quest

--- Quests/CoreQuests/Tier5Quest.cs
+++ Quests/CoreQuests/Tier5Quest.cs
@@ -9,22 +9,39 @@ namespace Expeditions.Quests
     {
         public override void SetDefaults()
         {
-            expedition.name = "Placeholder5";
+            expedition.name = "Soaring the Skies";
             expedition.difficulty = 5;
+            expedition.defeat = true;
             expedition.important = true;
+
+            expedition.conditionDescription1 = "Equip wings";
+
+            AddRewardItem(ItemID.GoldCoin, 5);
         }
         public override string Description(bool complete)
         {
-            return "";
-        }
-        public override void AddItemsOnLoad()
-        {
-
+            return "Have you seen white, snake-like beasts flying in the skies as of late? I've heard reports that they have harbour powerful souls which could make surviving against these new creatures much easier, if you could find a use for them. What do you suppose? ";
         }
 
         public override bool CheckPrerequisites(Player player)
         {
             return Expeditions.GetCurrentExpeditionTier() >= expedition.difficulty - 1;
+        }
+
+        public override bool CheckConditions(Player player, ref bool condition1, ref bool condition2, ref bool condition3)
+        {
+            if (!condition1)
+            {
+                foreach(Item i in player.miscEquips)
+                {
+                    if (i.wingSlot > 0)
+                    {
+                        condition1 = true;
+                        continue;
+                    }
+                }
+            }
+            return condition1;
         }
     }
 }
