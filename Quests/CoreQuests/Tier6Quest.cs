0588197be19131d6951798f8db4efd0040e13135
Flash Kirby <flashkirby@gmail.com>
2016-12-08 01:21:27 +0000

    Add tier 6 quest

--- Quests/CoreQuests/Tier6Quest.cs
+++ Quests/CoreQuests/Tier6Quest.cs
@@ -9,22 +9,45 @@ namespace Expeditions.Quests
     {
         public override void SetDefaults()
         {
-            expedition.name = "Placeholder5";
+            expedition.name = "Mecha Revenge";
             expedition.difficulty = 6;
+            expedition.defeat = true;
+            expedition.deliver = true;
             expedition.important = true;
+
+            expedition.conditionDescription1 = "Face against a mechanical boss";
+
+            expedition.AddDeliverable(ItemID.HallowedBar, 3);
+
+            AddRewardItem(ItemID.GoldCoin, 8);
         }
         public override string Description(bool complete)
         {
-            return "";
-        }
-        public override void AddItemsOnLoad()
-        {
-
+            return "I have a theory that there's some kind of hallowed equivalent to the " + (WorldGen.crimson ? "crimtane" : "demonite") + " metals that we found in said biome. Thing is, they dropped from bosses... so I wonder if there are counterparts to things like the Eye of Cthulu we haven't seen yet. ";
         }
 
         public override bool CheckPrerequisites(Player player)
         {
             return Expeditions.GetCurrentExpeditionTier() >= expedition.difficulty - 1;
+        }
+
+        public override bool CheckConditions(Player player, ref bool condition1, ref bool condition2, ref bool condition3)
+        {
+            if(!condition1 && !Main.dayTime)
+            {
+                foreach(NPC npc in Main.npc)
+                {
+                    if (npc.active && npc.boss)
+                    {
+                        condition1 = npc.type == NPCID.Spazmatism
+                            || npc.type == NPCID.Retinazer
+                            || npc.type == NPCID.TheDestroyer
+                            || npc.type == NPCID.SkeletronPrime;
+                        break;
+                    }
+                }
+            }
+            return condition1;
         }
     }
 }
