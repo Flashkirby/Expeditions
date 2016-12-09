ee9423d2b61d7725f760837f3e534621705d6b20
Flash Kirby <flashkirby@gmail.com>
2016-12-09 18:36:16 +0000

    Add Tier 10 quest

--- Quests/CoreQuests/Tier10Quest.cs
+++ Quests/CoreQuests/Tier10Quest.cs
@@ -9,22 +9,37 @@ namespace Expeditions.Quests
     {
         public override void SetDefaults()
         {
-            expedition.name = "Placeholder10";
+            expedition.name = "The Temple Conspiracy";
             expedition.difficulty = 10;
+            expedition.defeat = true;
             expedition.important = true;
+
+            expedition.conditionDescription1 = "Investigate the Jungle Temple";
+            expedition.conditionDescription2 = "Investigate the dungeon's mysterious coven";
+
+            AddRewardItem(ItemID.GoldCoin, 25);
+            AddRewardItem(ItemID.IronskinPotion, 10);
+            AddRewardItem(ItemID.EndurancePotion, 10);
         }
         public override string Description(bool complete)
         {
-            return "";
-        }
-        public override void AddItemsOnLoad()
-        {
-
+            if(expedition.condition1Met) return "Some strange people have moved into the dungeon ever since the golem was destroyed. They look real suspicious worshipping that tablet, what looks like it was plundered from the temple. But I would've thought sun worshippers would look, I dunno, more grossly incandescent?";
+            return "Have you explored much of that jungle temple yet? According to the artifacts so far, there seems to be some kind of lihzahrd fascination with, and worship of the sun. Maybe there's an altar hidden somewhere? ";
         }
 
         public override bool CheckPrerequisites(Player player)
         {
             return Expeditions.GetCurrentExpeditionTier() >= expedition.difficulty - 1;
+        }
+
+        public override bool CheckConditions(Player player, ref bool condition1, ref bool condition2, ref bool condition3)
+        {
+            if (!condition1) { condition1 = NPC.downedGolemBoss; }
+            else
+            {
+                if (!condition2) condition2 = Expeditions.LastHitNPC.type == NPCID.CultistBoss;
+            }
+            return condition1 && condition2;
         }
     }
 }
