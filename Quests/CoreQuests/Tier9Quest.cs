e2b49aa1c76e0a36dd9a93c0a15d7b0c77110dd4
Flash Kirby <flashkirby@gmail.com>
2016-12-09 18:01:29 +0000

    Add tier 9 Quest

--- Quests/CoreQuests/Tier9Quest.cs
+++ Quests/CoreQuests/Tier9Quest.cs
@@ -9,22 +9,37 @@ namespace Expeditions.Quests
     {
         public override void SetDefaults()
         {
-            expedition.name = "Placeholder9";
+            expedition.name = "Back to the Dungeons";
             expedition.difficulty = 9;
+            expedition.defeat = true;
             expedition.important = true;
+
+            expedition.conditionDescription1 = "Fight the Rusted Company";
+            expedition.conditionDescription2 = "Fight the Marching Bones";
+            expedition.conditionDescription3 = "Fight the Molten Legion";
+
+            AddRewardItem(ItemID.GoldCoin, 20);
         }
         public override string Description(bool complete)
         {
-            return "";
-        }
-        public override void AddItemsOnLoad()
-        {
-
+            return "You must've heard those spooky screams, right? Do skeletons even scream? That's for you to go down and find out! ";
         }
 
         public override bool CheckPrerequisites(Player player)
         {
             return Expeditions.GetCurrentExpeditionTier() >= expedition.difficulty - 1;
+        }
+
+        public override bool CheckConditions(Player player, ref bool condition1, ref bool condition2, ref bool condition3)
+        {
+            if(player.ZoneDungeon && !condition1 && !condition2 && !condition3)
+            {
+                NPC npc = Expeditions.LastHitNPC;
+                if (npc.type >= 269 && npc.type <= 272) condition1 = true; // Rusty Bones
+                if (npc.type >= 273 && npc.type <= 276) condition2 = true; // Blue Bones
+                if (npc.type >= 277 && npc.type <= 280) condition3 = true; // Hell Bones)
+            }
+            return condition1 && condition2 && condition3;
         }
     }
 }
