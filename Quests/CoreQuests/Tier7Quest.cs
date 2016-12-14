77228eb0f6de9af61a94bd514f66e8b2ee47e3bd
Flash Kirby <flashkirby@gmail.com>
2016-12-14 01:01:52 +0000

    Add tier 7 quest and rewards

--- Quests/CoreQuests/Tier7Quest.cs
+++ Quests/CoreQuests/Tier7Quest.cs
@@ -18,20 +18,31 @@ namespace Expeditions.Quests
         }
         public override void AddItemsOnLoad()
         {
-            expedition.AddDeliverable(ItemID.ChlorophyteOre, 3);
+            expedition.AddDeliverable(ItemID.ChlorophyteOre, 1);
+            expedition.AddDeliverable(ItemID.LifeFruit, 1);
 
             AddRewardItem(ItemID.GoldCoin, 10);
+            AddRewardItem(ItemID.GoldenCrate, 1);
+
+            // Find more life fruit and chlorophyte
+            AddRewardItem(ItemID.MiningPotion, 20);
+            AddRewardItem(ItemID.SpelunkerPotion, 20);
+
+            // Mushrooms for ranged stuff, enough for a small biome
+            AddRewardItem(ItemID.DarkBlueSolution, 20);
+
         }
         public override string Description(bool complete)
         {
-            return "I don't know how, but defeating those mechanical bosses seems to have triggered a sudden aggressiveness from the jungle. Don't suppose you could... take a peek? You'll be fine! ";
+            if (complete) return "Some kind of... super tough, organic ore? I wonder if it can grow like other plants? Oh and those life fruits - fascinating! Please, keep a lookout and see if there's anything else of note. I'd greatly appreciate it. ";
+            return "I don't know how, but defeating that mechanical boss seems to have triggered a certain... change from the jungle. Don't suppose you could take a peek? You'll be fine! ";
         }
 
         public override bool CheckPrerequisites(Player player)
         {
             return 
                 Expeditions.GetCurrentExpeditionTier() >= expedition.difficulty - 1
-                && NPC.downedMechBoss1 && NPC.downedMechBoss2 && NPC.downedMechBoss3;
+                && (NPC.downedMechBoss1 || NPC.downedMechBoss2 || NPC.downedMechBoss3);
         }
     }
 }
