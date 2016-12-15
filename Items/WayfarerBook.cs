59adce3d46bef5c2669b666fc6bdecbb51a8a512
Flash Kirby <flashkirby@gmail.com>
2016-12-15 00:17:21 +0000

    Add intial scripts for book and staff

---
+++ Items/WayfarerBook.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Expeditions.Items
+{
+    class WayfarerBook : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.WaterBolt);
+            item.name = "Wayfarer's Tome";
+            item.toolTip = "Casts a gust of wind";
+
+            item.mana = 7;
+            item.damage = 8;
+            item.useAnimation = 20;
+            item.useTime = 20;
+            item.knockBack = 8f;
+            item.shoot = ProjectileID.MagnetSphereBall;
+            item.shootSpeed = 11f;
+
+            item.value = Item.sellPrice(0, 3, 0, 0);
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2();
+        }
+    }
+}
---
+++ Items/WayfarerStaff.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Expeditions.Items
+{
+    class WayfarerStaff : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.AquaScepter);
+            item.name = "Wayfarer's Cane";
+            item.toolTip = "Shoots an explosive bolt";
+            item.width = 42;
+            item.height = 14;
+
+            item.mana = 12;
+            item.damage = 20;
+            item.useAnimation = 33;
+            item.useTime = 33;
+            item.knockBack = 3.5f;
+            item.shoot = ProjectileID.InfernoFriendlyBolt;
+            item.shootSpeed = 11f;
+
+            item.value = Item.sellPrice(0, 3, 0, 0);
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2();
+        }
+    }
+}
