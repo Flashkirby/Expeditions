2c4ff96ae48e88830161eaa699d8ad3fb83f07ec
Flash Kirby <flashkirby@gmail.com>
2016-12-16 02:56:35 +0000

    Add wind book projectile

--- Items/WayfarerBook.cs
+++ Items/WayfarerBook.cs
@@ -10,15 +10,16 @@ namespace Expeditions.Items
         public override void SetDefaults()
         {
             item.CloneDefaults(ItemID.WaterBolt);
-            item.name = "Wayfarer's Tome";
-            item.toolTip = "Casts a gust of wind";
+            item.name = "Wayfarer's Wind";
+            item.toolTip = "Casts a mighty gust of wind";
+            item.useSound = 34;
 
-            item.mana = 7;
+            item.mana = 20;
             item.damage = 8;
-            item.useAnimation = 20;
-            item.useTime = 20;
-            item.knockBack = 8f;
-            item.shoot = ProjectileID.MagnetSphereBall;
+            item.useAnimation = 26;
+            item.useTime = 26;
+            item.knockBack = 7f;
+            item.shoot = mod.ProjectileType("Gust");
             item.shootSpeed = 11f;
 
             item.value = Item.sellPrice(0, 3, 0, 0);
@@ -26,6 +27,19 @@ namespace Expeditions.Items
         public override Vector2? HoldoutOffset()
         {
             return new Vector2();
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Projectile.NewProjectile(position, new Vector2(
+                speedX + 2f * (Main.rand.NextFloat() - 0.5f),
+                speedY + 2f * (Main.rand.NextFloat() - 0.5f)
+                ), type, damage, knockBack, player.whoAmI);
+            Projectile.NewProjectile(position, new Vector2(
+                speedX + 4f * (Main.rand.NextFloat() - 0.5f),
+                speedY + 4f * (Main.rand.NextFloat() - 0.5f)
+                ), type, damage, knockBack, player.whoAmI);
+            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
         }
     }
 }
---
+++ Projs/Gust.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Expeditions.Projs
+{
+    class Gust : ModProjectile
+    {
+        public const int extraSize = 16;
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.WaterBolt);
+            projectile.aiStyle = 0;
+
+            projectile.name = "Gust";
+            projectile.width = 28 + extraSize;
+            projectile.height = 28 + extraSize;
+            projectile.timeLeft = 180;
+        }
+
+        public override void AI()
+        {
+            projectile.velocity *= 0.98f;
+
+            if ((int)projectile.ai[0] % 3 == 0)
+            {
+                int d = Dust.NewDust(projectile.position, projectile.width, projectile.height,
+                    16, projectile.velocity.X, projectile.velocity.Y, 200, default(Color), 0.7f);
+                Main.dust[d].fadeIn = 1.5f;
+                Main.dust[d].velocity *= 0.1f;
+
+                int alpha = (int)MathHelper.Clamp(255 - projectile.timeLeft, 0, 256);
+                int g = Gore.NewGore(new Vector2(
+                    projectile.position.X + Main.rand.Next(extraSize + 1), 
+                    projectile.position.Y + Main.rand.Next(extraSize + 1)), 
+                    default(Vector2), Main.rand.Next(11, 14), 1f);
+                Main.gore[g].velocity += projectile.velocity * 2;
+                Main.gore[g].velocity *= 0.2f;
+                Main.gore[g].alpha = alpha;
+            }
+
+            projectile.ai[0]++;
+
+            if (projectile.velocity.Length() < 0.1f || projectile.wet)
+            {
+                projectile.Kill();
+            }
+        }
+
+        public override void TileCollideStyle(ref int width, ref int height, ref bool fallThrough)
+        {
+            width = 8;
+            height = 8;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (projectile.velocity.X == 0) projectile.velocity.X = projectile.oldVelocity.X * -0.2f;
+            if (projectile.velocity.Y == 0) projectile.velocity.Y = projectile.oldVelocity.Y * -0.2f;
+            return false;
+        }
+    }
+}
